b87484ffac8985dd5e1a136659ca85ee6513fe68
DANIEL WILSON <daniel.j.wilson@btinternet.com>
2024-01-30 15:37:51 +0000

    added notes

--- client/main.js
+++ client/main.js
@@ -1,18 +1,19 @@
 const messageForm = document.getElementById("messageForm");
 
-messageForm.addEventListener("submit", handleSubmitMessageForm);
+messageForm.addEventListener("submit", handleSubmitMessageForm); //event listener for submit buttong
 
 function handleSubmitMessageForm(event) {
-  event.preventDefault();
+  event.preventDefault(); //prevents default settings !!!!REQUIRED!!!!!
 
   const formData = new FormData(messageForm);
   const message = formData.get("message");
 
+  // fetch request to local host 3000. setup on server side
   fetch("http://localhost:3000/messages", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ message }),
+    body: JSON.stringify({ message }), //stringify. JSON is async. so needs to be Stringified because we are POSTing. if GET then we use a async function
   });
 }
--- server/server.js
+++ server/server.js
@@ -6,12 +6,15 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+//express and cors setup
 
+//POST request aka POST method
 app.post("/messages", function (request, response) {
   console.log("request.body", request.body);
   response.json({ status: "Message received!" });
 });
 
 app.listen("3000", () => {
-  console.log("Server started on port 7070");
+  ///setting localhost to port 3000
+  console.log("Server started on port 3000");
 });
